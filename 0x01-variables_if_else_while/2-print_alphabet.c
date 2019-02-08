a886edf8e311c3255421cca546c67937b89aef8b
komerela <698@holbertonschool.com>
2019-02-08 22:39:24 +0000

    Print alphabet using c followed by new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Function prints alphabet
+ * creates a new line after the statement
+ *
+ */
+
+int main(void)
+
+{
+
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+
+		putchar(c);
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
