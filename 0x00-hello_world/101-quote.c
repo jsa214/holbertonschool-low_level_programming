96c911f63819baccbbbc7003cf5f208b760ff6ea
komerela <698@holbertonschool.com>
2019-02-09 08:24:56 +0000

    prints out piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Changes stderr without using printf or puts
+ *
+ * Return: 1
+ */
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+
+
+	return (1);
+}
