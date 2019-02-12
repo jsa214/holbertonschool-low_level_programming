1b1e247adde1194bdfb7d6563a91877b8b40122a
komerela <698@holbertonschool.com>
2019-02-12 22:17:51 +0000

    print alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *
+ * main = alphabet using lower case
+ *
+ * Return : void
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
