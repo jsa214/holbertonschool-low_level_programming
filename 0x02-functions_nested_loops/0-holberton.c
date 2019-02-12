51e08cf68b68605075a91858874823c9d638290c
komerela <698@holbertonschool.com>
2019-02-12 21:53:02 +0000

    Print string using putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *main - print string "Holberton"
+ *
+ *Return: 0
+ */
+
+int main()
+
+{
+	char string[] = "Holberton\n";
+	int index = 0;
+
+	while(string[index])
+
+	{
+		putchar(string[index]);
+		index++;
+
+	}
+
+	return (0);
+
+}
