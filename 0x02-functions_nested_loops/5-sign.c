f80ad1ad085dec2347a120c589398fdbbb49de9d
komerela <698@holbertonschool.com>
2019-02-13 07:44:55 +0000

    print positive or negative or zero with returns

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints logical sign as value
+ * @n: integer
+ * Return: '+' if n > 0 or '0' if = '0' otherwise '-'
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n < 48)
+	{
+		_putchar(45);
+		return (-1);
+
+	}
+
+	else
+	{
+		_putchar(48);
+		return (0);
+
+	}
+
+}
