33b04eb89aa8bc82631248b01117354a4a5ce210
komerela <698@holbertonschool.com>
2019-02-13 07:59:46 +0000

    changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,28 +5,30 @@
  *
  * @n: integer
  *
- * Return: '+' if n > 0 or '0' if = '0' otherwise '-'
+ * Return: 1 print + if n is positive
+ * Returns 0 print 0 if n is zero
+ * Returns -1 and print - if n is negative
  */
 
 int print_sign(int n)
 
 {
-	if (n > 48)
+	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
 
-	else if (n < 48)
+	else if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
 
 	}
 
 	else
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 
 	}
