8008b80d7c5977ad5b2fd6ce1e5f7fe97d01ab6b
komerela <698@holbertonschool.com>
2019-02-13 01:52:16 +0000

    print alphabet 10times in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - print alphabet 10 times
- * print_alphabet_x10 - print alphabet in lowercase
+ * print_alphabet - print alphabet in lowercase
  */
 
 void print_alphabet_x10(void)
