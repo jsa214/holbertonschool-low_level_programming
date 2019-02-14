d91b05a06cc76b1a96e6b4f5e94662aec6b9e0bb
komerela <rmwiti@gmail.com>
2019-02-14 06:44:12 +0000

    print factors of 1024 correction

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * printf - prints the value of multiples
- *
  * @ i: integer
  * @ j: integer
  *
- * Returns: 0
+ * Return: 0
  *
  */
-
-
-
+int main(void)
 {
-	int i, j = 0;
+	int i;
+	int j = 0;
 
 	for (i = 0; i < 1024; i++)
-
 		if (i % 3 == 0 || i % 5 == 0)
-
 			j = j + i;
-
 	printf("% i", j);
-
 	return (0);
-
 }
