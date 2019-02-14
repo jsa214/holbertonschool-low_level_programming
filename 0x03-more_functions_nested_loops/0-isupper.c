be76028ccd3f2bc90e588fd3ebe972362e912b80
komerela <rmwiti@gmail.com>
2019-02-14 21:04:18 +0000

    Find and print alphabet uppercase

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+# include "holberton.h"
+# include <stdio.h>
+
+/**
+ * _isupper - check for 'c' in uppercase
+ *
+ * @c: Variable to be checked
+ * Return: 0 if 'c' is uppercase, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
