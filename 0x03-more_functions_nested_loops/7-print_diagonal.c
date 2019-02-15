3f4730530204762e63ce0d755514a267f8967628
komerela <rmwiti@gmail.com>
2019-02-15 20:49:09 +0000

    Print diagonal line using putchar

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * void - print diagonal line
+ *
+ * @i - variable for columns
+ * @j - variable for rows
+ * Return : void
+ */
+
+void print_diagonal(int n)
+
+{
+
+	int i, j;
+
+	if (n > 0)
+	for (i = 0; i < n; i++)
+		for (j = 1; j <= i; j++)
+			if (i != j)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+	_putchar('\n');
+}
