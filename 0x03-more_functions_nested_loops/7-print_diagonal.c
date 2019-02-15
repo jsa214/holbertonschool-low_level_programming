cdd4e8465ac8019a6dbc56bc039bd88c46e93da6
komerela <rmwiti@gmail.com>
2019-02-15 23:49:58 +0000

    print diagonal line

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -13,20 +13,21 @@
 void print_diagonal(int n)
 
 {
-
 	int i, j;
 
 	if (n > 0)
-	for (i = 0; i < n; i++)
-		for (j = 1; j <= i; j++)
-			if (i != j)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
-	_putchar('\n');
+	{
+		for (i = 0; i < n; i++)
+			for (j = 1; j <= i; j++)
+				if (i != j)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+		_putchar('\n');
+	}
 }
