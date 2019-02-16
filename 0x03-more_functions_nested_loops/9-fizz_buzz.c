cd0364ef96f66c819177824cfb82a70e22195e52
komerela <rmwiti@gmail.com>
2019-02-16 09:20:59 +0000

    fizz buzz task to screen interviwees

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - function for fizz_buzz pattern
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 101)
+	{
+		if (i <= 100)
+		{
+			if ((i % 3 == 0) && (i % 5 == 0))
+				printf("FizzBuzz");
+			else if (i % 3 == 0)
+				printf("Fizz");
+			else if (i % 5 == 0)
+				printf("Buzz");
+			else
+				printf("%d", i);
+			if (i == 100)
+				break;
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
