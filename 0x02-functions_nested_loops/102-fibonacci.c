18d6a7502a581da157c28f71d3547b679efc5c95
komerela <rmwiti@gmail.com>
2019-02-19 22:41:54 +0000

    printing first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the value of multiples
+ *
+ * @i: integer representing where 1st number is
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int Fibonacci[15], i;
+
+	Fibonacci[0] = 0;
+	Fibonacci[1] = 1;
+
+	for ( i = 2; i < 50; ++i)
+		Fibonacci[i] = Fibonacci[i-2] + Fibonacci[i-1];
+
+	for (i = 1; i < 50; ++i)
+		printf ("%i\n, ", Fibonacci[i]);
+
+	return 0;
+
+}
