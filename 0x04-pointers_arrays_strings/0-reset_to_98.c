b5b6c80978ad40e78e24003a8d51227ae3aa19f9
komerela <rmwiti@gmail.com>
2019-02-19 06:28:05 +0000

    function takes int and updates

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function for pointers
+ *
+ * @n: variable describing *n
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
