cf4b729450a21d7e1b1e203944c040cd01a2667f
komerela <rmwiti@gmail.com>
2019-02-26 07:19:53 +0000

    pointer to break

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - pointer to break function
+ *
+ * @accept: string being checked for in string s
+ * @s: string to check
+ * Return: length i
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, k, len1 = 0, len2 = 0;
+
+	for (len1 = 0; s[len1] !='\0'; len1++)
+		for (len2 = 0; accept[len2] != '\0'; len2++)
+			for (i = 0; i < len1; i++)
+			{
+				for (j = 0; j < len2; j++)
+				{
+					if (s[i] == accept[j])
+					{
+						for (k = i; s[k] != '\0'; k++)
+							return (0);
+					}
+				}
+			}
+	return (NULL);
+}
