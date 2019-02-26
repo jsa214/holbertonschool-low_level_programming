c7fc86b20a36c5042cf9d65586aae74c0a905827
komerela <rmwiti@gmail.com>
2019-02-26 01:20:36 +0000

    copy memory area from src to dest using memcpy function

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
+	char *start = dest;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest = *src;
+		*dest++ = *src++;
 	}
-	return (dest);
+	return (start);
 }
