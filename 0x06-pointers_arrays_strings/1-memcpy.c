f3c4c03595f1a92ba8c21e5a0fbcdb76302c77d6
komerela <rmwiti@gmail.com>
2019-02-26 01:16:30 +0000

    correct file copy memory location from src to dest

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-	char *start = src;
 
 	for (i = 0; i < n; i++)
 	{
 		*dest = *src;
 	}
-	return (start);
+	return (dest);
 }
