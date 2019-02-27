38ce9b5cb442cc0678ee254e0404c88048fd3d11
komerela <rmwiti@gmail.com>
2019-02-27 07:09:04 +0000

    print sum of 2 diagonals

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print sum of the two diagonals of perfect matrix
+ *
+ * @a: pointer to the start of the matrix
+ * @size: size of matrix
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int sum1, sum2;
+
+	sum1 = 0;
+	for (i = 0; i < size; i++)
+		sum1 += a[i * size + 1];
+	printf("%d, ", sum1);
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+		sum2 += a[i * size + (size - i - 1)];
+	printf("%d\n", sum2);
+}
