fb555a6d80b2f866cbe67484fe5f1534c6150c06
komerela <rmwiti@gmail.com>
2019-02-27 09:30:56 +0000

    function that capitalizes all words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize first letter of each word in a string
+ * @s: string to operate on
+ *
+ * Return: updated string
+ */
+
+char *cap_string(char *s)
+{
+	char *temp;
+	int next;
+	int cap;
+
+	next = 1; /* First letter should be capitalized */
+	cap = 'a' - 'A';
+	for (temp = s; *temp != '\0'; temp++)
+	{
+		if (*temp == ' ' || *temp == '\t' || *temp == '\n' ||
+		    *temp == ',' || *temp == ';' || *temp == '.' || *temp == '!' ||
+		    *temp == '{' || *temp == '}')
+			next = 1;
+		else if (next)
+		{
+			next = 0;
+			if (*temp >= 'a' && *temp <= 'z')
+				*temp -= cap;
+		}
+	}
+	return (s);
+}
