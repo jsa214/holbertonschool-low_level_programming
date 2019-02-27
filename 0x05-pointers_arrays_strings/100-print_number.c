4576ffe75d2efb0c60076d614c7c8709354d3251
komerela <rmwiti@gmail.com>
2019-02-27 09:54:45 +0000

    print integer

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -16,9 +16,9 @@ void print_number(int n)
 		n *= -1;
 	else
 		_putchar('-');
-			if ((n / 10) != 0)
-				{
-					print_number((n / 10) * -1);
-				}
-				_putchar((n % 10) * -1 + '0');
+	if ((n / 10) != 0)
+	{
+		print_number((n / 10) * -1);
+	}
+	_putchar((n % 10) * -1 + '0');
 }
