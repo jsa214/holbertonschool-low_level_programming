9c1f9df60e49c8d0e08921bf2b606b1d0e201ef2
komerela <rmwiti@gmail.com>
2019-02-28 07:55:47 +0000

    power recursion

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - power recursion function
+ *
+ * @x: base
+ * @y: exponent
+ * Return: x pow y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (_pow_recursion(x, y));
+}
