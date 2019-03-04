c2c846333eb08d243a97964cdf103aa90c6d4525
komerela <rmwiti@gmail.com>
2019-03-04 06:29:09 +0000

    add two numbers from cmd line

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
-	{
-		printf("Error\n");
-		return (1);
-	}
-	result += atoi(argv[i]);
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
 	}
 	printf("%d\n", result);
 	return (0);
