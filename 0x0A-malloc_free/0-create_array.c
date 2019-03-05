72f88904e087b34c44d9c934f810300b1498ca37
komerela <rmwiti@gmail.com>
2019-03-05 23:48:28 +0000

    Update char str error to add pointer

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	str = malloc(size * sizeof(str));
+	str = malloc(size * sizeof(*str));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
