73e61995326b25b05d5dc9b480a40b0c3047bf6f
komerela <rmwiti@gmail.com>
2019-03-05 01:37:08 +0000

    add 2 numbers in the command line using argc and argv editing the braces

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -16,17 +16,17 @@ int main(int argc, char *argv[])
 	int result = 0;
 
 	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
 		{
-			for (j = 0; argv[i][j]; j++)
+			if (!isdigit(argv[i][j]))
 			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
 			result += atoi(argv[i]);
 		}
-	printf("%d\n", result);
-	return (0);
+	}
+		printf("%d\n", result);
+		return (0);
 }
