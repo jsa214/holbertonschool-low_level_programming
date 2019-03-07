395483bde1e046552b632c76d1d125570a4cd890
komerela <rmwiti@gmail.com>
2019-03-07 07:45:17 +0000

    update changes

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -4,4 +4,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *argstostr(int ac, char **av);
 #endif /*HOLBERTON_H*/
