311df6837b101510068fcd8911ff2148631f793d
komerela <rmwiti@gmail.com>
2019-03-08 07:57:16 +0000

    Adding stdio header

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * string_nconcat -Concatenates 2 strings
@@ -22,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < strlen1; i++)
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
 	{
 		if (i < strlen1)
 			p[i] = s1[i];
