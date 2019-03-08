478817b09ab5c193652d588d9a98c9b27cbc1a0a
komerela <rmwiti@gmail.com>
2019-03-08 07:46:14 +0000

    Changing the loop correction 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	p = malloc(nmemb * size);
-	if (p == NULL)
+	if (p == 0)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
 		p[i] = 0;
