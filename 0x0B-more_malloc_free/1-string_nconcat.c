b78442afa7d7e0461d69e48b63cacca0409421a7
komerela <rmwiti@gmail.com>
2019-03-08 07:53:17 +0000

    correction of spelling

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -22,9 +22,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	for (i = 0, c = 0; i < strlen1; i++)
 	{
-		if (i < strlen + n)
+		if (i < strlen1)
 			p[i] = s1[i];
 		else
 			p[i] = s2[c++];
