128a9bb5956ab6e8e22bca084dd559e87ca9ca03
komerela <rmwiti@gmail.com>
2019-03-12 05:17:38 +0000

    Define a new type

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog: mentioning descriptions
+ *
+ * @name: Char type
+ * @owner: Character type
+ * @age: float type
+ *
+ * Description: longer description
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
