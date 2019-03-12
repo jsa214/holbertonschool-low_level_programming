dc6a9a90ecdab31010428b6b5c0092eb7bbe21b9
komerela <rmwiti@gmail.com>
2019-03-12 05:23:58 +0000

    Define a new type Struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog: mentioning descriptions
+ * struct dog- mentioning descriptions
  *
  * @name: Char type
  * @owner: Character type
