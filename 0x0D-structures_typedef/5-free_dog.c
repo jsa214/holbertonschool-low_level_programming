36a624312f911c20929ce36fdcca5c25cfcbd27c
komerela <rmwiti@gmail.com>
2019-03-13 06:43:22 +0000

    Free pointer to struct added null ifcond

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+
 
 /**
  * free_dog - function creates dog
  *
  * @d: parameter pointing to struct dog
+ * Return: void
  */
 
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
