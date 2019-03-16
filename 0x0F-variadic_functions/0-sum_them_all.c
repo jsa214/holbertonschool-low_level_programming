429d6fb4d19a105e4eaf8b48f6a5bd6680138710
komerela <rmwiti@gmail.com>
2019-03-16 20:09:23 +0000

    Added header stdarg

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
@@ -7,6 +6,7 @@
  * sum_them_all - Function adds all the arguments
  *
  * @n: place holder for arguments
+ * *...: many more arguments to sum
  * Return: Sum
  */
 
@@ -15,21 +15,19 @@
 int sum_them_all(const unsigned int n, ...)
 
 /* va_list declares a variable, args, which we use to manipulate arglist */
-/* va_start line init args variable with function's last fixed argument n */
-/* va_arg() fetches the next argument form the argument list */
 {
 	va_list args;
 
-	unsigned int i = 0;
-	unsigned int sum = 0;
-
+	unsigned int i, sum = 0;
+/* va_start line init args variable with function's last fixed argument n */
 	va_start(args, n);
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
-
+/* va_arg() fetches the next argument form the argument list */
+		sum = (sum + va_arg(args, int));
+/* va_end(args) ends the function */
 	va_end(args);
 	return (sum);
 }
