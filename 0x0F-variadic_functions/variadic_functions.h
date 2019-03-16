3157f94aaed4b77a2726995fa46bd1b3af6d3199
komerela <rmwiti@gmail.com>
2019-03-16 20:08:31 +0000

    Updated variadic functions header with type struc for va_list

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,7 +1,27 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdargs.h>
+#include <stdio.h>
+
+/**
+ * struct print_functions - Struct print_functions
+ *
+ * @type: Format to print
+ * @f: The print function to use
+ */
+typedef struct print_functions
+{
+	char *type;
+	void (*f)();
+}print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif /*VARIADIC_FUNCTIONS_H
+void print_number(va_list args);
+void print_string(va_list args);
+void print_char(va_list args);
+void print_float(va_list args);
+
+#endif /*VARIADIC_FUNCTIONS_H*/
