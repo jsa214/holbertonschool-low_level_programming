593acfcf749556a05297ddf4a41a21b49ce4ceca
komerela <rmwiti@gmail.com>
2019-03-18 07:14:50 +0000

    Upload file with correct header file

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdlib.h>
 
 /* define the format string */
 
 /**
  * print_char - print char
- *
- * @c: format specifier for char
- * @va_arg: Variadic function for arguments
+ * @args: Arguments
+ * Return: void
  */
 
 void print_char(va_list args)
@@ -18,9 +18,8 @@ void print_char(va_list args)
 
 /**
  * print_integer - print integer
- *
- * @i: format specifier for integer
- * @va_arg: Variadic function for arguments
+ * @args: arguments
+ * Return: void
  */
 
 void print_integer(va_list args)
@@ -30,9 +29,8 @@ void print_integer(va_list args)
 
 /**
  * print_float - print float
- *
- * @f: format specifier for float
- * @va_arg: Variadic function for arguments
+ * @args: arguments
+ * Return: void
  */
 
 void print_float(va_list args)
@@ -42,14 +40,14 @@ void print_float(va_list args)
 
 /**
  * print_string - print string
- *
- * @s: format specifier for string
- * @va_arg: Variadic function for arguments
+ * @args: arguments
+ * Return: void
  */
 
 void print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
+
 	if (s == '\0')
 		s = "(nil)";
 	printf("%s", s);
@@ -62,30 +60,38 @@ void print_string(va_list args)
 
 void print_all(const char * const format, ...)
 {
+/* Define the array of structures */
+	print_t structs_ar[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 /* Initialize the list */
 
 	va_list args;
+	char *separator;
+	unsigned int i, j;
+
 	va_start(args, format);
-	char *separator = "";
-	unsigned int i = 0, j = 0;
-/* Define the array of structures */
-
-print_t structs_ar[] = {
-	{"c", print_char},
-	{"i", print_integer},
-	{"f", print_float},
-	{"s", print_string},
-};
-
-i = 0;
-while (format != 0 && format[i / 4] != 0)
-{
-	j = 0;
-	while (structs_ar[j])
-		printf("%s", separator);
+	i = 0;
+	separator = "";
+	while (format != '\0' && format[i] != '\0')
 	{
-		if (format[i] == structs_ar[j].type)
-		    structs_ar[j].f(args);
-		separator = ", ";
-	} j++;
-} i++;
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *structs_ar[j].type)
+			{
+				printf("%s", separator);
+				structs_ar[j].f(args);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
