dcc65087e001f2ee1a5041c203112ab998566ead
komerela <rmwiti@gmail.com>
2019-03-21 16:27:12 +0000

    Return length

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+size_t list_len(const list_t *h)
+{
+	size_t list_len(str *);
+	int count(str *);
+
+	int count = 0;
+	str *h = head;
+
+	while (str != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return(count);
+}
