24eed45c733e330b2c0df9164bc68ac793b373f1
komerela <rmwiti@gmail.com>
2019-03-21 16:29:47 +0000

    Updated header with new prototypes

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/* Typedef used to describe data types */
+/* Typedef used to describe data structure */
 
 /**
  * list_t - Struct list_t
@@ -12,11 +12,15 @@
  */
 
 typedef struct list_t {
-unsigned int data;
-struct list_t *next;
-} List;
+	int data;
+	struct list_t *next;
+} str;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 int _putchar(char c);
 
 #endif /*LISTS_H*/
