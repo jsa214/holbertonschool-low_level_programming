7c4fad3aef05a51153f709beedd3e367ddee6534
komerela <rmwiti@gmail.com>
2019-03-21 19:58:40 +0000

    Function that returns the count of nodes

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -2,15 +2,18 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * list_len - Get the length of the list in list_h
+ *
+ * @h: Head of list
+ * Return: count of nodes
+ */
+
 size_t list_len(const list_t *h)
 {
-	size_t list_len(str *);
-	int count(str *);
+	size_t count = 0;
 
-	int count = 0;
-	str *h = head;
-
-	while (str != NULL)
+	while (h != NULL)
 	{
 		count++;
 		h = h->next;
